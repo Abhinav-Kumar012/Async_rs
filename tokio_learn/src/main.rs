ff577331685702284e9b5598cc993bfbed894167
Abhinav Kumar <01abk33+github@gmail.com>
2026-05-29 09:36:45 +0530

    learning tokio - 2

--- tokio_learn/src/main.rs
+++ tokio_learn/src/main.rs
@@ -1,5 +1,14 @@
+use tokio::io::{self, AsyncWriteExt};
+
+async fn run() {
+	let mut cout = io::stdout();
+	_ = cout.write_all(b"hello, Async!\n").await;
+	_ = cout.flush().await;
+}
+
+
 
 #[tokio::main]
 async fn main() {
-	println!("Hello, world!");
+	_ = run().await;
 }
